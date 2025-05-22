12f5decd0c49b04a9a250a1cd9e5e3d3472f9f26
HardyUX <hartwigbthelen@gmail.com>
2025-05-22 14:05:11 +0200

    feat: Resized use template button and changed the icon

--- src/components/MealForm.jsx
+++ src/components/MealForm.jsx
@@ -233,9 +233,11 @@ function MealForm() {
                                             <button
                                                 onClick={() => setActiveTemplateTargetDate(dateString)}
                                                 title="Use Template"
-                                                className="bg-yellow-400 hover:bg-yellow-500 text-white px-3 py-2 rounded"
+                                                className="use-template-btn mt-2 bg-yellow-400 hover:bg-yellow-500 text-white px-4 py-2 rounded"
                                             >
-                                                📄
+                                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
+                                                    <path strokeLinecap="round" strokeLinejoin="round" d="M19.5 14.25v-2.625a3.375 3.375 0 0 0-3.375-3.375h-1.5A1.125 1.125 0 0 1 13.5 7.125v-1.5a3.375 3.375 0 0 0-3.375-3.375H8.25m3.75 9v6m3-3H9m1.5-12H5.625c-.621 0-1.125.504-1.125 1.125v17.25c0 .621.504 1.125 1.125 1.125h12.75c.621 0 1.125-.504 1.125-1.125V11.25a9 9 0 0 0-9-9Z" />
+                                                </svg>
                                             </button>
                                         </div>
                                         
