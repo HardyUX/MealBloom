d056ac5c3c2df05eeaafa8ad654d12d8c8dc3940
HardyUX <hartwigbthelen@gmail.com>
2025-05-22 23:24:00 +0200

    feat: add highlight border to current date in both Views and visually highlight weekends in Month View

--- src/components/DropZone.jsx
+++ src/components/DropZone.jsx
@@ -1,12 +1,13 @@
 import { useMealDrop } from '../hooks/useMealDrop';
 
-function DropZone({ dateString, children, onMealDrop }) {
+function DropZone({ dateString, children, onMealDrop, highlight }) {
     const [{ isOver }, drop] = useMealDrop(onMealDrop, dateString);
 
     return (
         <div
             ref={drop}
-            className={`meal-day-container ${isOver ? 'bg-blue-100' : ''}`}
+            className={
+                `meal-day-container ${isOver ? 'bg-blue-100' : ''} ${highlight ? " ring-2 ring-blue-400" : ""}`}
         >
             {children}
         </div>
